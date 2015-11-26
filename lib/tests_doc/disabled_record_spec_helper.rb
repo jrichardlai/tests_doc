12edac63f7e75c84db7323fcf904226b6a314aee
Jean-Richard Lai <jrichardlai@gmail.com>
2015-11-26 11:28:43 -0800

    Move Repo

--- lib/tests_doc/disabled_record_spec_helper.rb
+++
@@ -1,7 +0,0 @@
-module TestsDoc
-  module DisabledRecordSpecHelper
-    def recording_api_interaction(*)
-      yield OpenStruct.new
-    end
-  end
-end
--- lib/tests_doc/version.rb
+++
@@ -1,3 +0,0 @@
-module TestsDoc
-  VERSION = "0.0.1"
-end
